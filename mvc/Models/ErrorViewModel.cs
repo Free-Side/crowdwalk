308b58b605e3ba6ec532c81c6bc2f21823bac895
Paul Wheeler <sflanker@hotmail.com>
2021-07-29 22:27:45 +0000

    Removed mvc folder

--- mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace SampleWebAPI.Models {
-  public class ErrorViewModel {
-    public string RequestId { get; set; }
-
-    public bool ShowRequestId => !String.IsNullOrEmpty(RequestId);
-  }
-}
